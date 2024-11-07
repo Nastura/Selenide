36f8aedead1589ee1ce37fb1e79f2e5f7107df15
Nastura <116873621+Nastura@users.noreply.github.com>
2024-11-07 10:38:08 +0300

    Update Data.java

--- src/main/java/ru/netology/Data.java
+++ src/main/java/ru/netology/Data.java
@@ -4,11 +4,6 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class Data {
-    public String formatPlus(long day) {
-        LocalDate today = LocalDate.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        LocalDate newDay = today.plusDays(day);
-        return newDay.format(formatter);
-        } 
+
     }
 
